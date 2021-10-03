81b726f0314a075527f5809cffd5841496112930
JinPark <jin225675@naver.com>
2021-10-03 19:02:23 +0900

    baekjoon 5338 solve

---
+++ baekjoon_etc/baekjoon5338.java
@@ -0,0 +1,17 @@
+package baekjoon_etc;
+
+import java.io.*;
+
+public class baekjoon5338 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+        bw.flush();
+    }
+}
