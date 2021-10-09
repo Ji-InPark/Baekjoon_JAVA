1d5f55e748eba57c27dcf71a8c570d6a462fc43f
JinPark <jin225675@naver.com>
2021-10-10 00:54:34 +0900

    baekjoon 2420 solve

---
+++ baekjoon_etc/baekjoon2420.java
@@ -0,0 +1,24 @@
+package baekjoon_etc;
+
+import java.io.*;
+
+public class baekjoon2420 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+
+        long start = Long.parseLong(input[0]), end = Long.parseLong(input[1]);
+
+        if(start > end)
+        {
+            long temp = end;
+            end = start;
+            start = temp;
+        }
+
+        bw.write((end - start) + "\n");
+        bw.flush();
+    }
+}
